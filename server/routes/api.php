162edec0802eef1afcf418a076889f3e3cf30f00
sahan99827 <111554133+sahan99827@users.noreply.github.com>
2023-07-24 14:25:44 +0530

    Initial commit

---
+++ server/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChangePasswordController;
+use App\Http\Controllers\ResetPasswordController;
+
+Route::group([
+
+    'middleware' => 'api',
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('signup', [AuthController::class,'signup']);
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::post('phone', [AuthController::class,'phone']);
+    Route::post('laptop', [AuthController::class,'laptop']);
+    Route::post('vehicle', [AuthController::class,'vehicle']);
+    Route::post('home', [AuthController::class,'home']);
+    Route::post('me', [AuthController::class,'me']);
+    Route::post('sendPasswordResetLink', [ResetPasswordController::class,'sendEmail']);
+    Route::post('resetPassword', [ChangePasswordController::class,'process']);
+
+});
+
